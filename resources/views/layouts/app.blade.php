3e49b63de0f7b508e8df1011ee80358a572b9ecc
Paul Holt <paul-ho@lightflows.co.uk>
2023-04-17 19:45:19 +0100

    Install livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     <title>@yield('title')</title>
+    @livewireStyles
 </head>
 <body class="h-full w-full bg-primaryBackground">
     @yield('content')
+    @livewireScripts
 </body>
 </html>
