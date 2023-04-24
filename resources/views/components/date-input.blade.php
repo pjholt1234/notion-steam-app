9e17f221bbf0d0318ecdc776d88c4c075ae1ae45
Paul Holt <paul-ho@lightflows.co.uk>
2023-04-24 21:51:22 +0100

    Fix date component

--- resources/views/components/date-input.blade.php
+++ resources/views/components/date-input.blade.php
@@ -4,11 +4,15 @@
     'errorMsg' => '',
 ])
 
+@php
+    $defaultAttributes = [];
+@endphp
+
 <x-field-wrapper
     :name="$name"
     :label="$slot"
     :error="$error"
     :errorMsg="$errorMsg"
 >
-    <input type="date" name="{{$name}}"/>
+    <input type="date" name="{{$name}}" {{ $attributes->merge($defaultAttributes) }}/>
 </x-field-wrapper>
