598938ecc9ec11c703bb0feaba20e25cb653cd93
Paul Holt <paul-ho@lightflows.co.uk>
2023-05-01 18:13:14 +0100

    Update modal event and styling

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,21 +1,21 @@
 @props([
-    'title' => null
+    'title' => null,
 ])
 @php
-    $defaultAttributes = [];
+    $defaultAttributes = ['class' => 'overflow-hidden'];
 @endphp
 <div
-    {{$attributes->merge($defaultAttributes)}}
+    {{$attributes->except('class')}}
     x-data="{show: true}"
     x-show="show"
     x-init="
       eventName = $el.id+'-toggle';
-      window.addEventListener(eventName, event => show = true);
+      window.addEventListener(eventName, event => show = !show);
     "
     class="fixed inset-0 z-50 overflow-y-auto"
 >
-    <div class="flex items-center justify-center min-h-screen">
-        <x-card class="overflow-hidden w-[500px] h-[400px]">
+    <div class="flex items-center justify-center min-h-screen bg-gray-500 bg-opacity-50">
+        <x-card {{$attributes->except('id')->merge($defaultAttributes)}} >
             <div class="text-white mb-4 flex">
                 @if(isset($title))
                     <h2 class="text-xl font-bold">{{ $title }}</h2>
