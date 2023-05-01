9984735c3f9c703e2dde5c7584b9aeb30e3b96ec
Paul Holt <paul-ho@lightflows.co.uk>
2023-05-01 18:16:31 +0100

    Add steam market modal to purchase page

--- app/Http/Livewire/CreatePurchase.php
+++ app/Http/Livewire/CreatePurchase.php
@@ -27,6 +27,11 @@ class CreatePurchase extends FormAbstract
         'transaction_date' => null,
     ];
 
+    public function openSearch()
+    {
+        $this->dispatchBrowserEvent('add-item-modal-toggle');
+    }
+
     public function submit()
     {
         $this->validate($this->rules);
--- resources/views/purchases/create.blade.php
+++ resources/views/purchases/create.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 @section('title', 'Add purchase')
 @section('content')
-<x-card>
+<x-modal id="add-item-modal" class="h-[300px] w-[600px]" title="Add Steam Item">
+    <livewire:create-steam-item />
+</x-modal>
+
+<div class="grid grid-cols-2">
     <livewire:create-purchase />
-</x-card>
+</div>
 
 @endsection
