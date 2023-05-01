eabb5fe127ea2d08a8dbda21ec9016fc1f0b87b4
Paul Holt <paul-ho@lightflows.co.uk>
2023-05-01 18:43:42 +0100

    Fix z indexing issue

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -4,7 +4,7 @@
 ])
 
 @php
-    $defaultAttributes = ['class' => 'absolute h-[60px] inset-0 m-2 p-2 cursor-pointer rounded flex items-center text-white'];
+    $defaultAttributes = ['class' => 'z-50 fixed h-[60px] inset-0 m-2 p-2 cursor-pointer rounded flex items-center text-white'];
 @endphp
 <div {{$attributes->merge($defaultAttributes)}}
      x-data="{ show: false, onEvent: {{json_encode($onEvent)}}, message: '', success: true}"
@@ -16,7 +16,7 @@
             success = {{json_encode($success)}};
         }
      "
-     @alert.window ="
+     @alert.window="
         show = true
         setTimeout(() => show = false, 3000)
         message = $event.detail.message;
--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -13,15 +13,15 @@
       eventName = $el.id+'-toggle';
       window.addEventListener(eventName, event => show = !show);
     "
-    class="fixed inset-0 z-50 overflow-y-auto"
+    class="fixed inset-0 z-20 overflow-y-auto bg-gray-500 bg-opacity-50"
 >
-    <div class="flex items-center justify-center min-h-screen bg-gray-500 bg-opacity-50">
+    <div class="z-20 flex items-center justify-center min-h-screen">
         <x-card {{$attributes->except('id')->merge($defaultAttributes)}} >
             <div class="text-white mb-4 flex">
                 @if(isset($title))
                     <h2 class="text-xl font-bold">{{ $title }}</h2>
                 @endif
-                    <x-button class="flex text-sm border border-red-500 hover:bg-red-500 w-[30px] mr-0 ml-auto" @click="show = false">X</x-button>
+                <x-button class="flex text-sm border border-red-500 hover:bg-red-500 w-[30px] mr-0 ml-auto" @click="show = false">X</x-button>
             </div>
             <div>
                 {{ $slot }}
